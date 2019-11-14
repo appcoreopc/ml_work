c9d271d8bf5dc2a6479310a545dfda65453e30d8
jeremy woo <kepungmoin@hotmail.com>
2019-11-14 18:35:35 +1300

    added code

--- tf/gradient_boost/README.md
+++ tf/gradient_boost/README.md
@@ -39,3 +39,50 @@ for feature_name in CATEGORICAL_COLUMNS:
 for feature_name in NUMERIC_COLUMNS:
   feature_columns.append(tf.feature_column.numeric_column(feature_name,
                                            dtype=tf.float32))
+
+example = dict(dftrain.head(1))
+class_fc = tf.feature_column.indicator_column(tf.feature_column.categorical_column_with_vocabulary_list('class', ('First', 'Second', 'Third')))
+print('Feature value: "{}"'.format(example['class'].iloc[0]))
+print('One-hot encoded: ', tf.keras.layers.DenseFeatures([class_fc])(example).numpy())
+
+tf.keras.layers.DenseFeatures(feature_columns)(example).numpy()
+
+# Use entire batch since this is such a small dataset.
+NUM_EXAMPLES = len(y_train)
+
+def make_input_fn(X, y, n_epochs=None, shuffle=True):
+  def input_fn():
+    dataset = tf.data.Dataset.from_tensor_slices((dict(X), y))
+    if shuffle:
+      dataset = dataset.shuffle(NUM_EXAMPLES)
+    # For training, cycle thru dataset as many times as need (n_epochs=None).
+    dataset = dataset.repeat(n_epochs)
+    # In memory training doesn't use batching.
+    dataset = dataset.batch(NUM_EXAMPLES)
+    return dataset
+  return input_fn
+
+# Training and evaluation input functions.
+
+train_input_fn = make_input_fn(dftrain, y_train)
+eval_input_fn = make_input_fn(dfeval, y_eval, shuffle=False, n_epochs=1)
+
+
+## using LinearClassifier
+
+linear_est = tf.estimator.LinearClassifier(feature_columns)
+
+# Train model.
+linear_est.train(train_input_fn, max_steps=100)
+
+# Evaluation.
+result = linear_est.evaluate(eval_input_fn)
+clear_output()
+print(pd.Series(result))
+
+
+
+
+
+
+
--- tf/gradient_boost/boost.py
+++ tf/gradient_boost/boost.py
@@ -5,6 +5,11 @@ import pandas as pd
 from IPython.display import clear_output
 from matplotlib import pyplot as plt
 
+
+import tensorflow as tf
+
+tf.enable_eager_execution()
+
 # Load dataset.
 dftrain = pd.read_csv('https://storage.googleapis.com/tf-datasets/titanic/train.csv')
 dfeval = pd.read_csv('https://storage.googleapis.com/tf-datasets/titanic/eval.csv')
@@ -39,3 +44,59 @@ for feature_name in CATEGORICAL_COLUMNS:
 for feature_name in NUMERIC_COLUMNS:
   feature_columns.append(tf.feature_column.numeric_column(feature_name,
                                            dtype=tf.float32))
+
+example = dict(dftrain.head(1))
+class_fc = tf.feature_column.indicator_column(tf.feature_column.categorical_column_with_vocabulary_list('class', ('First', 'Second', 'Third')))
+print('Feature value: "{}"'.format(example['class'].iloc[0]))
+print('One-hot encoded: ', tf.keras.layers.DenseFeatures([class_fc])(example).numpy())
+
+tf.keras.layers.DenseFeatures(feature_columns)(example).numpy()
+
+# Use entire batch since this is such a small dataset.
+NUM_EXAMPLES = len(y_train)
+
+def make_input_fn(X, y, n_epochs=None, shuffle=True):
+  def input_fn():
+    dataset = tf.data.Dataset.from_tensor_slices((dict(X), y))
+    if shuffle:
+      dataset = dataset.shuffle(NUM_EXAMPLES)
+    # For training, cycle thru dataset as many times as need (n_epochs=None).
+    dataset = dataset.repeat(n_epochs)
+    # In memory training doesn't use batching.
+    dataset = dataset.batch(NUM_EXAMPLES)
+    return dataset
+  return input_fn
+
+# Training and evaluation input functions.
+
+train_input_fn = make_input_fn(dftrain, y_train)
+eval_input_fn = make_input_fn(dfeval, y_eval, shuffle=False, n_epochs=1)
+
+
+## using LinearClassifier
+
+linear_est = tf.estimator.LinearClassifier(feature_columns)
+
+# Train model.
+linear_est.train(train_input_fn, max_steps=100)
+
+# Evaluation.
+result = linear_est.evaluate(eval_input_fn)
+clear_output()
+print(pd.Series(result))
+
+
+print("using boost ")
+# Since data fits into memory, use entire dataset per layer. It will be faster.
+# Above one batch is defined as the entire dataset.
+n_batches = 1
+est = tf.estimator.BoostedTreesClassifier(feature_columns,
+                                          n_batches_per_layer=n_batches)
+
+# The model will stop training once the specified number of trees is built, not
+# based on the number of steps.
+est.train(train_input_fn, max_steps=100)
+
+# Eval.
+result = est.evaluate(eval_input_fn)
+clear_output()
